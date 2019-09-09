1ff2d85311a008fb6a0f0ab7077b3109a24ebb47
Miranda J. Ignacio <mirandajignacio@gmail.com>
2019-09-09 17:36:14 -0300

    Update initial-data.js

--- src/initial-data.js
+++ src/initial-data.js
@@ -2,7 +2,7 @@ const initialData = {
   tasks: {
     "task-1": { id: "task-1", content: "Amanecer con un cerdito" },
     "task-2": { id: "task-2", content: "Dormir mas de 6hs" },
-    "task-3": { id: "task-3", content: "LLorar por el Vitel Toné", type: "BUG" },
+    "task-3": { id: "task-3", content: "LLorar por el Vitel Toné" },
     "task-4": { id: "task-4", content: "Ordenar la pieza", type: "BUG" }
   },
   columns: {
